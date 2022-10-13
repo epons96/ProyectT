b7f3faf06615b6711c3020fc91463782d45807f9
Eduardo Pérez Pons <epons9611@gmail.com>
2022-10-13 00:51:20 -0400

    Actualizando diseño Cart

--- src/app/client/cart/cart.module.ts
+++ src/app/client/cart/cart.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CartComponent } from "./cart.component";
 import { CartRoutingModule } from "./cart-routing.module";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
   declarations: [CartComponent],
-  imports: [CommonModule, CartRoutingModule],
+  imports: [CommonModule, CartRoutingModule, FlexLayoutModule],
 })
 export class CartModule {}
