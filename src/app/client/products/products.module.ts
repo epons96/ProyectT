9daedca0f22440a585eeed56c881df9f44a611a1
Eduardo Pérez Pons <epons9611@gmail.com>
2022-10-13 00:52:41 -0400

    Iniciando diseño del producto

--- src/app/client/products/products.module.ts
+++ src/app/client/products/products.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ProductLayoutComponent } from "./product-layout/product-layout.component";
 import { ProductsRoutingModule } from "src/app/client/products/products-routing.module";
-import { ProductComponent } from './product/product.component';
+import { ProductComponent } from "./product/product.component";
+import { MatIconModule } from "@angular/material/icon";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
   declarations: [ProductLayoutComponent, ProductComponent],
-  imports: [CommonModule, ProductsRoutingModule],
+  imports: [
+    CommonModule,
+    ProductsRoutingModule,
+    MatIconModule,
+    FlexLayoutModule,
+  ],
 })
 export class ProductsModule {}
